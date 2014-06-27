d10bd43c11ea3bc75f7b0f7e4b6a0174ebf5c67f
Demir Selmanovic <dselmanovic@gmail.com>
2014-06-27 18:33:37 +0200

    new bearerJs with callbacks

--- app.js
+++ app.js
@@ -27,22 +27,45 @@ bearer({
     //Please change server key for your own safety!
     serverKey:"12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678",
     tokenUrl:'/token', //Call this URL to get your token. Accepts only POST method
-    createToken:function(req){
+    extendTokenUrl:'/extendtoken', //Call this URL to get your token. Accepts only POST method
+    createToken:function(req, next, cancel){
         //If your user is not valid just return "underfined" from this method.
         //Your token will be added to req object and you can use it from any method later
         var username=req.body.username;
-        var userValid=true; //You are aware that this is where you check username/password in your DB, right!?
-        if (userValid) return({
-            expire: moment(Date.now()).add('days', 1).format('YYYY-MM-DD HH:mm:ss'),
-            username: username,
-            contentType: req.get('Content-Type'),
-            ip: req.ip,
-            userAgent: req.header('user-agent'),
-            custom_id: '55555',
-            another: 'Some data you need in your token',
-            moreData: 'Some more data you need'
-        });
-        return undefined;
+        //var password=req.body.password;
+
+        //You get the idea how to use next and cancel callbacks, right?
+        if (true){
+            next({
+                expire: moment(Date.now()).add('days', 1).format('YYYY-MM-DD HH:mm:ss'),
+                username: username,
+                contentType: req.get('Content-Type'),
+                ip: req.ip,
+                userAgent: req.header('user-agent'),
+                custom_id: '55555',
+                another: 'Some data you need in your token',
+                moreData: 'Some more data you need'
+            });
+        }else{
+            cancel();
+        }
+    },
+    extendToken:function(req, next, cancel){
+        var token=req.authToken;
+        if (token){
+            next({
+                expire: moment(Date.now()).add('days', 1).format('YYYY-MM-DD HH:mm:ss'),
+                username: token.username,
+                contentType: req.get('Content-Type'),
+                ip: req.ip,
+                userAgent: req.header('user-agent'),
+                custom_id: '55555',
+                another: 'Some data you need in your token',
+                moreData: 'Some more data you need'
+            });
+        }else{
+            cancel();
+        }
     },
     validateToken:function(req, token){
         //you could also check if request came from same IP using req.ip==token.ip for example
@@ -51,23 +74,35 @@ bearer({
         }
         return false;
     },
-    onTokenValid:function(token){
+    onTokenValid:function(token, next, cancel){
         //This is in case you would like to check user account status in DB each time he attempts to do something.
         //Doing this will affect your performance but its your choice if you really need it
         //Returning false from this method will reject user even if his token is OK
-        return true;
+        var username=token.username;
+        if (true){
+            next()
+        }else{
+            cancel();
+        }
     },
-    userInRole:function(token, role){
+    userInRole:function(token, roles, next, cancel){
         //Provide role level access restrictions on url
         //You can use onTokenValid for this also, but I find this easier to read later
         //If you specified "roles" property for any secureRoute below, you must implement this method
-        return true;
+        var username=token.username;
+
+        if (true){
+            next();
+        }else
+        {
+            cancel();
+        }
     },
     onAuthorized: function(req, token){
-        console.log("this will be executed if request is OK");
+        //console.log("this will be executed if request is OK");
     },
     onUnauthorized: function(req, token){
-        console.log("this will be executed if request fails authentication");
+        //console.log(req.path, "this will be executed if request fails authentication");
     },
     secureRoutes:[
         {url:'/secure', method:'get'},
@@ -77,6 +112,6 @@ bearer({
 });
 
 //Setup routing
-var routeConfig = require('./routes/route-config')(app);
+require('./routes/route-config')(app);
 
 module.exports = app;
